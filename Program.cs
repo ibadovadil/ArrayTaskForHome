efb3455ddd1a3d49543d7854770b1b91528a6732
Adil <adilibadov654@gmail.com>
2023-10-10 23:05:48 +0400

    Added Homework on Arrays

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ArrayHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Array = { 1, 7, 16, 5, 9 };
+
+            Console.Write("Original Array: [");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(Array[i]);
+                if (i < Array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+
+
+            int start = 0;
+            int end = Array.Length - 1;
+            while (start < end)
+            {
+                int temp = Array[start];
+                Array[start] = Array[end];
+                Array[end] = temp;
+
+                start++;
+                end--;
+            }
+
+            Console.Write("Reversed Array: [");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(Array[i]);
+                if (i < Array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
